67d9ae1567bfb4539382e575e6007eac938f1622
mirian320 <sylvamirian54@gmail.com>
2022-09-14 17:14:00 -0700

    Signa

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - Prints the sign of a number
+ *
+ * @n: is the argument to be passed in he function
+ *
+ * Return: 1 and prints + if n is greater than zero,
+ * 0 and prints 0 if n is zero,
+ * -1 and prints - if n is less than zero
+ */
+int print_sign(int n)
+{
+_putchar('+');
+return (1);
+}
+else if (n < 0)
+{
+_putchar('-');
+return (-1);
+}
+else
+{
+_putchar('0');
+return (0);
+}
+}
+
