be14c97214cdee1881587ac05a995dc763559598
mirian320 <sylvamirian54@gmail.com>
2022-09-14 17:42:23 -0700

    what a thing is worth to you

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/**
+ * _abs - returns absolute value of n
+ *@n: integer value to be tested
+ *Return: absolute value of n
+ *
+ */
+
+int _abs(int n)
+{
+if (n >= 0)
+return (n);
+}
