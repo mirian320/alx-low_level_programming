d84f65cca84de0a796e06edf5173cf3e074e21cf
mirian320 <sylvamirian54@gmail.com>
2022-09-23 15:49:20 -0700

    magic

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -8,7 +8,6 @@ int *p;
 
 a[2] = 1024;
 p = &n;
-
 /*
  * write your line of code here...
  * Remember:
@@ -21,4 +20,5 @@ p = &n;
 /* ...so that this prints 98\n */
 printf("a[2] = %d\n", a[2]);
 return (0);
+
 }
